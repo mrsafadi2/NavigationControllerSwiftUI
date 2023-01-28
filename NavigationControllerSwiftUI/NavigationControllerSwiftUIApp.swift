71a38233bc8b76d6c35e2ff43d8390a12719b217
mrsafadi2 <mrsafadi2@gmail.com>
2023-01-28 14:48:17 +0200

    Initial Commit

---
+++ NavigationControllerSwiftUI/NavigationControllerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationControllerSwiftUIApp.swift
+//  NavigationControllerSwiftUI
+//
+//  Created by Mohammed Safadi Macbook Pro on 28/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct NavigationControllerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
